3cad7a5847ad7ba50f5fb6264f6bf96070fc2732
dan1sh15 <danishqayyum9793@gmail.com>
2023-09-21 12:49:13 +0530

    Added API fetch method to get random quotes.

--- script.js
+++ script.js
@@ -7,104 +7,90 @@ const btn = document.querySelector('.btn');
 const quoteIcon = document.querySelector('.quote-icon');
 const tweet = document.querySelector('.tweet-icon');
 const body = document.body;
-let news = [
+let colorsArr = [
     {
-      "text": "Genius is one percent inspiration and ninety-nine percent perspiration.",
-      "author": "Thomas Edison",
       "color": "rgb(52, 34, 36)"
     },
     {
-      "text": "You can observe a lot just by watching.",
-      "author": "Yogi Berra",
       "color": "rgb(44, 62, 80)"
     },
     {
-      "text": "A house divided against itself cannot stand.",
-      "author": "Abraham Lincoln",
       "color": "rgb(22, 160, 133)"
     },
     {
-      "text": "Difficulties increase the nearer we get to the goal.",
-      "author": "Johann Wolfgang von Goethe",
       "color": "rgb(251, 105, 100)"
     },
     {
-      "text": "Fate is in your hands and no one elses",
-      "author": "Byron Pulsifer",
       "color": "rgb(119, 177, 169)"
     },
     {
-      "text": "Be the chief but never the lord.",
-      "author": "Lao Tzu",
       "color": "rgb(71, 46, 50)"
     },
     {
-      "text": "Nothing happens unless first we dream.",
-      "author": "Carl Sandburg",
       "color": "rgb(189, 187, 153)"
     },
     {
-      "text": "Well begun is half done.",
-      "author": "Aristotle",
       "color": "rgb(39, 174, 96)"
     },
     {
-      "text": "Life is a learning experience, only if you learn.",
-      "author": "Yogi Berra",
       "color": "rgb(119, 177, 169)"
     },
     {
-      "text": "Self-complacency is fatal to progress.",
-      "author": "Margaret Sangster",
       "color": "rgb(44, 62, 80)"
     },
     {
-      "text": "Peace comes from within. Do not seek it without.",
-      "author": "Buddha",
       "color": "rgb(119, 177, 169)"
     },
     {
-      "text": "What you give is what you get.",
-      "author": "Byron Pulsifer",
       "color": "rgb(231, 76, 60)"
     },
     {
-      "text": "We can only learn to love by loving.",
-      "author": "Iris Murdoch",
       "color": "rgb(251, 105, 100)"
     },
     {
-      "text": "Life is change. Growth is optional. Choose wisely.",
-      "author": "Karen Clark",
       "color": "rgb(119, 177, 169)"
     },
     {
-      "text": "You'll see it when you believe it.",
-      "author": "Wayne Dyer",
       "color": "rgb(39, 174, 96)"
     },
     {
-      "text": "Today is the tomorrow we worried about yesterday.",
-      "author": "type.fit",
       "color": "rgb(155, 89, 182)"
     }
   ]
+
 
 function getRandomIndex(array) {
     let randomIndex = Math.floor(Math.random() * array.length);
     return randomIndex;
 }
 
-btn.addEventListener('click', () => {
-    let randomIdx = getRandomIndex(news);
-    quote.innerHTML = news[randomIdx].text;
-    author.innerHTML = "-by " + news[randomIdx].author;
-    wrapper.style.backgroundColor = news[randomIdx].color;
-    text.style.color = news[randomIdx].color;
-    author.style.color = news[randomIdx].color;
-    btn.style.backgroundColor = news[randomIdx].color;
-    tweet.style.backgroundColor = news[randomIdx].color;
-    quoteIcon.style.color = news[randomIdx].color;
-    wrapper.style.transition = "all 1s ease-in-out";
-    quoteBox.style.transition = "all 0.5s ease-in-out";
-})
+
+// function to fetch quote from the API
+async function fetchQuote() {
+  let response = await fetch(`https://api.quotable.io/quotes/random?maxLength=100`);
+  let data = await response.json();
+  // console.log(data);
+  // console.log(data[0].content);
+  // console.log(data[0].author);
+
+  return data;
+}
+
+btn.addEventListener('click', async () => {
+
+    let data = await fetchQuote();
+    let newQuote = data[0].content;
+    let newAuthor = data[0].author;
+
+    let randomIdx = getRandomIndex(colorsArr);
+    quote.innerHTML = newQuote;
+    author.innerHTML = "-by " + newAuthor;
+    wrapper.style.backgroundColor = colorsArr[randomIdx].color;
+    text.style.color = colorsArr[randomIdx].color;
+    author.style.color = colorsArr[randomIdx].color;
+    btn.style.backgroundColor = colorsArr[randomIdx].color;
+    tweet.style.backgroundColor = colorsArr[randomIdx].color;
+    quoteIcon.style.color = colorsArr[randomIdx].color;
+    wrapper.style.transition = "background-color 0.8s ease-in-out";
+    quoteBox.style.transition = "color 0.5s ease-in-out";
+});
